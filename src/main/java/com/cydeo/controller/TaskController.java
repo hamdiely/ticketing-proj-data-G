bc21399a09d16ec1ae670a9a42681790c1ccf1ba
Mohamed <hamdiely@gmail.com>
2022-11-10 02:02:34 -0500

    validation

--- src/main/java/com/cydeo/controller/TaskController.java
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -39,7 +39,7 @@ public class TaskController {
     }
 
     @PostMapping("/create")
-    public String insertTask(/*@Valid*/ @ModelAttribute("task") TaskDTO task, BindingResult bindingResult, Model model) {
+    public String insertTask(@Valid @ModelAttribute("task") TaskDTO task, BindingResult bindingResult, Model model) {
 
         if (bindingResult.hasErrors()) {
 
